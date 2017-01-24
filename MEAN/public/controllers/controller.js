4aaf18780fb3e7d559a580c38a2c7cd2403a75c2
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2017-01-24 18:20:29 +0000

    account users

--- MEAN/public/controllers/controller.js
+++ MEAN/public/controllers/controller.js
@@ -5,7 +5,7 @@ myApp.controller('mainController' , function($scope ,$http){
     console.log("Hello From controller");
     
 var refresh = function(){
-     $http.get('/contactlist').success(function(response){
+     $http.get('/accoutUsers').success(function(response){
         console.log("I got the data i requested");
         $scope.contactlist = response;
          //empty the boxes 
@@ -21,7 +21,7 @@ refresh();
         //name of the database and what we have to send 
         //and you get back response as well 
         //which is on console you can see by pressing ctrl+shift+j
-        $http.post('/contactlist' , $scope.contact).success(function(response){
+        $http.post('/accoutUsers' , $scope.contact).success(function(response){
             console.log(response);
             //straight away call refresh function to load data after pressing the add contact button
             refresh();
@@ -32,7 +32,7 @@ refresh();
  $scope.remove= function(id){
      console.log(id);
      //call the refresh function after delete the record so its reload the new data from mongo db.
-     $http.delete('/contactlist/'+id).success(function(response){
+     $http.delete('/accoutUsers/'+id).success(function(response){
          refresh();
      });
  };  
@@ -42,7 +42,7 @@ refresh();
 //takes the object id and put that contact in input boxes
 $scope.edit = function(id){
     console.log(id);
-    $http.get('/contactlist/'+ id).success(function(response){
+    $http.get('/accoutUsers/'+ id).success(function(response){
         //this line will put the response what we got from mongodb to the input boxes .
        $scope.contact = response; 
     });
@@ -55,7 +55,7 @@ $scope.edit = function(id){
 $scope.update = function(){
     console.log($scope.contact._id);
     //send to the server whatever we have edited
-    $http.put('/contactlist/' + $scope.contact._id,$scope.contact).success(function(response){
+    $http.put('/accoutUsers/' + $scope.contact._id,$scope.contact).success(function(response){
         refresh();
     })
 };
--- MEAN/server.js
+++ MEAN/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express(); //we will be using the express.js
 var mongojs = require('mongojs');//require the mongodb module 
-var db = mongojs('contactlist',['contactlist']); //which mongodb database we will be using 
+var db = mongojs('globtrotter',['globtrotter']); //which mongodb database we will be using 
 
 //now we have to require body parser 
 var bodyParser = require('body-parser');
@@ -12,33 +12,30 @@ app.use(express.static(__dirname +"/public"));
 //so that our app can use bodyparser
 app.use(bodyParser.json());
 
-app.get('/contactlist' , function(req, res){
+app.get('/accoutUsers' , function(req, res){
     console.log("I received a GET request");
     
     //getting the data from mongodb
-    db.contactlist.find(function(err , docs){
+    db.accoutUsers.find(function(err , docs){
         console.log(docs);
         res.json(docs);
-    });
-    
-  
-    
+    }); 
 });
 
 
-app.post('/contactlist' , function(req , res){
+app.post('/accoutUsers' , function(req , res){
     // To parse the data and server doent know how to parse , we  need to install another module called " body parser" .so lets intall it from command promt by typing npm install body parser 
     console.log(req.body);
-    db.contactlist.insert(req.body ,function(err , doc){
+    db.accoutUsers.insert(req.body ,function(err , doc){
         res.json(doc);
     })
 });
 
-app.delete('/contactlist/:id',function(req , res){
+app.delete('/accoutUsers/:id',function(req , res){
     var id = req.params.id;
     console.log(id);
     //pass the id to the mongodb to delete the exact record we want to delete and get the data back 
-    db.contactlist.remove({_id:mongojs.ObjectId(id)},function(err , doc){
+    db.accoutUsers.remove({_id:mongojs.ObjectId(id)},function(err , doc){
         res.json(doc);
     })
     
@@ -46,10 +43,10 @@ app.delete('/contactlist/:id',function(req , res){
 
 
 //after update the contact we need to get it back as well
-app.get('/contactlist/:id' ,function(req ,res){
+app.get('/accoutUsers/:id' ,function(req ,res){
     var id = req.params.id;
     console.log(id);
-    db.contactlist.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
+    db.accoutUsers.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
        res.json(doc); 
     });
     
@@ -57,19 +54,15 @@ app.get('/contactlist/:id' ,function(req ,res){
 
 
 //use http put request to change the current contact
-app.put('/contactlist/:id',function(req ,res){
+app.put('/accoutUsers/:id',function(req ,res){
    var id = req.params.id;
     console.log(req.body.name);
-    db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+    db.accoutUsers.findAndModify({query: {_id: mongojs.ObjectId(id)},
              update: {$set: {name: req.body.name , email: req.body.email, number: req.body.number}},
              new: true},function(err ,doc){
                     res.json(doc);
-        });
-     
+        });    
 });
-
-
-
 
 
 app.listen(3000);
