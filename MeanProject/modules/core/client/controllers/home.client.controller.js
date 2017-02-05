df6b6e7969c77fc96fce50cb75dbd16bef23f33a
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2017-02-05 22:41:54 +0000

    Edit HomePage

--- MeanProject/modules/core/client/controllers/home.client.controller.js
+++ MeanProject/modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,46 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+	
+	$scope.events = [
+		{
+			img: 'glyphicon-eye-open',
+			color: 'btn-primary',
+			title: 'Search Events',
+			description: 'description'
+		},
+		{
+			img: 'glyphicon-eye-open',
+			color: 'btn-primary',
+			title: 'Search Events',
+			description: 'description'
+		},
+		{
+			img: 'glyphicon-calendar',
+			color: 'btn-info',
+			title: 'Search Events',
+			description: 'description'
+		},
+		{
+			img: 'glyphicon-eye-open',
+			color: 'btn-primary',
+			title: 'Search Events',
+			description: 'description'
+		},
+		{
+			img: 'glyphicon-user',
+			color: 'btn-danger',
+			title: 'Search Events',
+			description: 'description'
+		},
+		{
+			img: 'glyphicon-eye-open',
+			color: 'btn-warning',
+			title: 'Search Events',
+			description: 'description'
+		}
+	];
+	
+	
   }
 ]);
