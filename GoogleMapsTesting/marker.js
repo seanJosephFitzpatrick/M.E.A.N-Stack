44e2d57ae3774c5f499e6540b933d06db399120a
JasonThorne <Jason@JAY-PC>
2017-02-05 17:59:30 +0000

    importing multiple markers

--- GoogleMapsTesting/marker.js
+++ GoogleMapsTesting/marker.js
@@ -1,4 +1,25 @@
+eqfeed_callback
+(
+    {
+        "type":"FeatureCollection","features":
+        [
+            {
+              "type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[ 53.2714495477047, -9.056854245546901]}
+            },
+            {
+              "type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[55.1557, 3.4067]}
+            }
+        ]
+    }
+)
+    
+
+
+
+
+/*
 eqfeed_callback({"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[ 53.2714495477047, -9.056854245546901]}}]})
+*/
 
 /*
 eqfeed_callback({"type":"FeatureCollection","features":[{"type":"Feature","properties":{"mag":5.4,"place":"48km SSE of Pondaguitan, Philippines","time":1348176066,"tz":480,"url":"http://earthquake.usgs.gov/earthquakes/eventpage/usc000csx3","felt":2,"cdi":3.4,"mmi":null,"alert":null,"status":"REVIEWED","tsunami":null,"sig":"449","net":"us","code":"c000csx3","ids":",usc000csx3,","sources":",us,","types":",dyfi,eq-location-map,general-link,geoserve,historical-moment-tensor-map,historical-seismicity-map,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,tectonic-summary,"},"geometry":{"type":"Point","coordinates":[126.3832,5.9775,111.16]},"id":"usc000csx3"}]})
