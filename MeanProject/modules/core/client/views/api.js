f9a392a5242fe1e9fce19b7eabe001de7206ead7
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2017-02-28 22:56:36 +0000

    Added button on Homepage to search Eventful API

--- MeanProject/modules/core/client/views/api.js
+++ MeanProject/modules/core/client/views/api.js
@@ -1,56 +1,36 @@
-function show_alert(){
-
-  var oArgs = {
-
-            app_key:"DjPHQVFWsH5q7D85",
-
-            id: "20218701",
-
-           // page_size: 25 ,
-
-  };
-
-  EVDB.API.call("/events/get", oArgs, function(oData) {
-  
-
-      // Note: this relies on the custom toString() methods below
-	  
-	  	var myJSON = JSON.stringify(oData);
+ function loaddata() {
+        //get vars 
+        var oArgs = {
 		
-		//console.log(myJSON);
+			app_key: "DjPHQVFWsH5q7D85",
+
+			  q: "music",
+
+			  where: "Galway, Ireland", 
+
+			  sort_order: "popularity",
+        };
+
+        var content = '';
+
+        //API CALL
+        EVDB.API.call("/events/search", oArgs, function(oData) {
+            console.log(oData)
+			
+			var myJSON = JSON.stringify(oData);
 		
-		var obj = angular.fromJson(myJSON);
-		
-		console.log("Postal code = ", obj.postal_code);
-		console.log("Latitude = ", obj.latitude);
+			console.log(myJSON);
 
-	});	
-}
+            //Get the title for each item
+            for (var i = 0;i < oData.events.event.length;i++) {
+                content += "Title: " + oData.events.event[i].title + " City: " 
+				+ oData.events.event[i].city_name +  " Latitude: "
+				+ oData.events.event[i].latitude + " Lonitude: "
+				+ oData.events.event[i].longitude +
+				'<br><br>';
+            }
 
-function show_alert2() {
-   var oArgs = {
-
-      app_key: "DjPHQVFWsH5q7D85",
-
-      q: "music",
-
-      where: "Galway, Ireland", 
-
-      "date": "2017013100-2017022000",
-
-      page_size: 5,
-
-      sort_order: "popularity",
-
-   };
-
-   EVDB.API.call("/events/search", oArgs, function(oData) {
-		
-      // Note: this relies on the custom toString() methods below
-		
-		var myJSON = JSON.stringify(oData);
-		
-		console.log(myJSON);
-
-	});
-}
+            // Show Data on page
+            $("#ListItems").html(content);
+        });
+    }
