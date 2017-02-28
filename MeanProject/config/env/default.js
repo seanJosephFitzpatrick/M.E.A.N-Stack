d44f218bfbd2e7240d5a27bd031ebe9f4bf9fe4c
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2017-02-28 17:31:17 +0000

    Add Image feature to Events

--- MeanProject/config/env/default.js
+++ MeanProject/config/env/default.js
@@ -29,11 +29,17 @@ module.exports = {
   sessionCollection: 'sessions',
   logo: 'modules/core/client/img/brand/logo.png',
   favicon: 'modules/core/client/img/brand/favicon.ico',
-  uploads: {
+ uploads: {
     profileUpload: {
       dest: './modules/users/client/img/profile/uploads/', // Profile upload destination path
       limits: {
         fileSize: 1*1024*1024 // Max file size in bytes (1 MB)
+      }
+    },
+    fileUpload: {
+      dest: './modules/core/client/img/uploads/', // file upload destination path
+      limits: {
+        fileSize: 10*1024*1024 // Max file size in bytes (10 MB)
       }
     }
   }
